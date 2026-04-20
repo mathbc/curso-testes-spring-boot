80e3c2b371c590470d2c73966090f8847ff8a842
Matheus Araújo <matheus@primeinternacional.com.br>
2026-04-20 15:28:30 -0300

    Iniciado os testes integrados para subcutaneos

---
+++ sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
@@ -0,0 +1,15 @@
+package com.example.swplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class PlanetIT {
+    @Test
+    public void contextLoads() {
+
+    }
+}
