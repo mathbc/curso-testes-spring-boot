80be1e0f6a8cbf6ee5571d57c7c9d6f165c81343
Matheus Araújo <matheus@primeinternacional.com.br>
2026-04-20 18:56:58 -0300

    Adicionado mais testes subcutaneos

--- sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
+++ sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
@@ -1,15 +1,49 @@
 package com.example.swplanetapi;
 
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static com.example.swplanetapi.common.PlanetConstants.TATOOINE;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import com.example.swplanetapi.domain.Planet;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/import_planets.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = {"/remove_planets.sql"}, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 public class PlanetIT {
-    @Test
-    public void contextLoads() {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    public void createPlanet_ReturnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody()).isNotNull();
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+    }
+
+    @Test
+    public void getPlanet_RetunsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/1", Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody().getName()).isEqualTo(TATOOINE.getName());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(TATOOINE.getTerrain());
+        assertThat(sut.getBody().getClimate()).isEqualTo(TATOOINE.getClimate());
     }
 }
--- sw-planet-api/src/test/java/com/example/swplanetapi/common/PlanetConstants.java
+++ sw-planet-api/src/test/java/com/example/swplanetapi/common/PlanetConstants.java
@@ -10,8 +10,8 @@ public class PlanetConstants {
   public static final Planet INVALID_PLANET = new Planet("", "", "");
 
   public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
-  public static final Planet ALDERAAN = new Planet(1L, "Alderaan", "temperate", "gass");
-  public static final Planet YAVINIV = new Planet(1L, "Yavin IV", "temperate, tropical", "gass");
+  public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "temperate", "gass");
+  public static final Planet YAVINIV = new Planet(3L, "Yavin IV", "temperate, tropical", "gass");
 
   public static final List<Planet> PLANETS = new ArrayList<>() {
     {
